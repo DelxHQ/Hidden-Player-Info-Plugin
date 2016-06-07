1fa597a39a0dd413a4c7010000c179e0b1af6bc0
ArceusMatt <arceusthegod303@yahoo.com>
2016-06-07 04:51:05 -0400

    Create Main.php
    Main created, keep in mind it's my first time and many errors will be known.

---
+++ src/HiddenInfo/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace HiddenInfo;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\Player;
+
+class Main extends PluginBase {
+    public function onEnable() {
+        $this->getLogger()->info(Color::BLUE."[HiddenInfo] Enabled successfully.");
+    }
+    public function onDisable() {
+        $this->getLogger()->info(Color::BLUE."[HiddenInfo] Disabled successfully.");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        if($sender instanceof Player) {
+            $name = $sender->getName();
+            if(strtolower($command->getName()) == 'getinfo') {
+                if(count($args) > 0) {
+                    $player = $args[0];
+                if($this->getServer()->getPlayer($player)) {
+                   $player = $this->getServer()->getPlayer($player);
+                   $cid = $player->getClientId();
+                   $ip = $player->getAddress();
+                   $port = $player->getPort();
+                   $sender->sendMessage(Color::BLUE."[INFO] ".$player->getName()." CID: $cid IP: $ip Port: $port");
+                   return;
+                  } else {
+                      $sender->sendMessage(Color::RED."[INFO] Invaild player or wrong syntax.");
+                   return;
+                  }
+                } else {
+                    $sender->sendMessage(Color::BLUE."[INFO] ".$player->getName()." CID: $cid IP: $ip Port: $port");
+                   return;
+                }
+            }
+        }
+        $sender->sendMessage(Color::BLUE."[INFO] ".$player->getName()." CID: $cid IP: $ip Port: $port");
+                   return;
+    }
+}
